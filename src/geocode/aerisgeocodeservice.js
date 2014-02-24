cdbdb3045f0727db96bf713dcfb5a52ee3601b09
chicks <chicks@weathernation.net>
2014-02-24 14:13:31 -0600

    Reformatting code to 2 spaces.

--- src/geocode/aerisgeocodeservice.js
+++ src/geocode/aerisgeocodeservice.js
@@ -1,44 +1,44 @@
 define([
-    'aeris/util',
-    'aeris/config',
-    'aeris/jsonp',
-    'aeris/maps/strategy/utils',
-    'aeris/promise',
-    'aeris/geocode/geocodeserviceresponse',
-    'aeris/geocode/geocodeservicestatus'
+  'aeris/util',
+  'aeris/config',
+  'aeris/jsonp',
+  'aeris/maps/strategy/utils',
+  'aeris/promise',
+  'aeris/geocode/geocodeserviceresponse',
+  'aeris/geocode/geocodeservicestatus'
 ], function(_, AerisConfig, JSONP, mapUtil, Promise, GeocodeServiceResponse, GeocodeServiceStatus) {
-    /**
-     * @class aeris.geocode.AerisGeocodeService
-     * @implements aeris.geocode.GeocodeServiceInterface
-     * @constructor
-     */
-    var AerisGeocodeService = function(opt_options) {
-        var options = _.defaults(opt_options || {}, {
-            jsonp: JSONP
-        });
-        this.serviceUrl_ = 'http://api.aerisapi.com/places';
-        this.jsonp_ = options.jsonp;
+  /**
+   * @class aeris.geocode.AerisGeocodeService
+   * @implements aeris.geocode.GeocodeServiceInterface
+   * @constructor
+   */
+  var AerisGeocodeService = function(opt_options) {
+    var options = _.defaults(opt_options || {}, {
+      jsonp: JSONP
+    });
+    this.serviceUrl_ = 'http://api.aerisapi.com/places';
+    this.jsonp_ = options.jsonp;
+  };
+
+  AerisGeocodeService.prototype.geocode = function(location) {
+    var promise = new Promise();
+    var query = {
+      client_id: AerisConfig.get('apiId'),
+      client_secret: AerisConfig.get('apiSecret'),
+      p: location
     };
 
-    AerisGeocodeService.prototype.geocode = function(location) {
-        var promise = new Promise();
-        var query = {
-            client_id: AerisConfig.get('apiId'),
-            client_secret: AerisConfig.get('apiSecret'),
-            p: location
-        };
+    this.jsonp_.get(this.serviceUrl_, query, _.bind(function(res) {
+      if (!res || res.error || !res.success) {
+        promise.reject(res);
+      }
+      else {
+        promise.resolve(res);
+      }
+    }, this));
 
-        this.jsonp_.get(this.serviceUrl_, query, _.bind(function(res) {
-            if (!res || res.error || !res.success) {
-                promise.reject(res);
-            }
-            else {
-                promise.resolve(res);
-            }
-        }, this));
+    return promise;
+  };
 
-        return promise;
-    };
-
-    return _.expose(AerisGeocodeService, 'aeris.geocode.AerisGeocodeService');
+  return _.expose(AerisGeocodeService, 'aeris.geocode.AerisGeocodeService');
 });
--- tests/spec/aeris/geocode/aerisgeocodeservice.js
+++ tests/spec/aeris/geocode/aerisgeocodeservice.js
@@ -1,114 +1,114 @@
 define([
-    'aeris/util',
-    'mocks/aeris/config',
-    'mocks/aeris/jsonp',
-    'aeris/geocode/geocodeservicestatus',
-    'aeris/geocode/aerisgeocodeservice'
+  'aeris/util',
+  'mocks/aeris/config',
+  'mocks/aeris/jsonp',
+  'aeris/geocode/geocodeservicestatus',
+  'aeris/geocode/aerisgeocodeservice'
 ], function(_, MockConfig, MockJSONP, GeocodeServiceStatus, AerisGeocodeService) {
 
-    var aerisService, mockJSONP;
+  var aerisService, mockJSONP;
+
+  beforeEach(function() {
+    mockJSONP = new MockJSONP();
+    aerisService = new AerisGeocodeService({
+      jsonp: mockJSONP
+    });
+
+    MockConfig.stubApiKeys();
+  });
+
+  afterEach(function() {
+    MockConfig.restore();
+  });
+
+  function getSuccessResponse() {
+    return {
+      'success': true,
+      'error': null,
+      'response': {
+        'loc': {
+          'lat': 44.97997,
+          'long': -93.26384
+        },
+        'place': {
+          'name': 'Minneapolis',
+          'state': 'MN',
+          'stateFull': 'Minnesota',
+          'country': 'US',
+          'countryFull': 'United States',
+          'region': 'usnc',
+          'regionFull': 'North Central',
+          'continent': 'nam',
+          'continentFull': 'North America'
+        },
+        'profile': {
+          'elevM': 253,
+          'elevFT': 830,
+          'pop': 382578,
+          'tz': 'America\/Chicago',
+          'tzname': 'CST',
+          'tzoffset': -21600,
+          'isDST': false
+        }
+      }
+    };
+  }
+
+  function getErrorResponse() {
+    return {
+      'success': false,
+      'error': {
+        'code': 'invalid_location',
+        'description': 'The requested location was not found.'
+      },
+      'response': []
+    };
+  }
+
+  describe('The AerisGeocodeService', function() {
+    var location, aerisUrl, callSpy, errSpy;
 
     beforeEach(function() {
-        mockJSONP = new MockJSONP();
-        aerisService = new AerisGeocodeService({
-            jsonp: mockJSONP
-        });
-
-        MockConfig.stubApiKeys();
+      location = 'somewhere';
+      aerisUrl = 'http://api.aerisapi.com/places';
     });
 
-    afterEach(function() {
-       MockConfig.restore();
+    it('should query the Aeris places api', function() {
+      callSpy = jasmine.createSpy('callSpy').andCallFake(function() {
+        expect(mockJSONP.getRequestedUrl()).toBe(aerisUrl);
+      });
+
+      // mock aeris geocode
+      aerisService.geocode().done(callSpy);
+      mockJSONP.resolveWith(getSuccessResponse());
+
+      expect(callSpy).toHaveBeenCalled();
+      expect(callSpy).toHaveBeenCalledWith(getSuccessResponse());
     });
 
-    function getSuccessResponse() {
-        return {
-            'success': true,
-            'error': null,
-            'response': {
-                'loc': {
-                    'lat': 44.97997,
-                    'long': -93.26384
-                },
-                'place': {
-                    'name': 'Minneapolis',
-                    'state': 'MN',
-                    'stateFull': 'Minnesota',
-                    'country': 'US',
-                    'countryFull': 'United States',
-                    'region': 'usnc',
-                    'regionFull': 'North Central',
-                    'continent': 'nam',
-                    'continentFull': 'North America'
-                },
-                'profile': {
-                    'elevM': 253,
-                    'elevFT': 830,
-                    'pop': 382578,
-                    'tz': 'America\/Chicago',
-                    'tzname': 'CST',
-                    'tzoffset': -21600,
-                    'isDST': false
-                }
-            }
-        };
-    }
+    it('should handle api errors', function() {
+      errSpy = jasmine.createSpy('errSpy').andCallFake(function() {
+        expect(mockJSONP.getRequestedUrl()).toBe(aerisUrl);
+      });
 
-    function getErrorResponse() {
-        return {
-            'success': false,
-            'error': {
-                'code': 'invalid_location',
-                'description': 'The requested location was not found.'
-            },
-            'response': []
-        };
-    }
+      aerisService.geocode().fail(errSpy);
+      mockJSONP.resolveWith(getErrorResponse());
 
-    describe('The AerisGeocodeService', function() {
-        var location, aerisUrl, callSpy, errSpy;
-
-        beforeEach(function() {
-            location = 'somewhere';
-            aerisUrl = 'http://api.aerisapi.com/places';
-        });
-
-        it('should query the Aeris places api', function() {
-            callSpy = jasmine.createSpy('callSpy').andCallFake(function() {
-                expect(mockJSONP.getRequestedUrl()).toBe(aerisUrl);
-            });
-
-            // mock aeris geocode
-            aerisService.geocode().done(callSpy);
-            mockJSONP.resolveWith(getSuccessResponse());
-
-            expect(callSpy).toHaveBeenCalled();
-            expect(callSpy).toHaveBeenCalledWith(getSuccessResponse());
-        });
-
-        it('should handle api errors', function() {
-            errSpy = jasmine.createSpy('errSpy').andCallFake(function() {
-                expect(mockJSONP.getRequestedUrl()).toBe(aerisUrl);
-            });
-
-            aerisService.geocode().fail(errSpy);
-            mockJSONP.resolveWith(getErrorResponse());
-
-            expect(errSpy).toHaveBeenCalled();
-            expect(errSpy).toHaveBeenCalledWith(getErrorResponse());
-        });
-
-        it('should return successful api responses', function() {
-            callSpy = jasmine.createSpy('callSpy').andCallFake(function() {
-                expect(mockJSONP.getRequestedUrl()).toBe(aerisUrl);
-            });
-
-            // mock aeris geocode
-            aerisService.geocode().done(callSpy);
-            mockJSONP.resolveWith(getSuccessResponse());
-
-            expect(callSpy).toHaveBeenCalled();
-            expect(callSpy).toHaveBeenCalledWith(getSuccessResponse());
-        });
+      expect(errSpy).toHaveBeenCalled();
+      expect(errSpy).toHaveBeenCalledWith(getErrorResponse());
     });
+
+    it('should return successful api responses', function() {
+      callSpy = jasmine.createSpy('callSpy').andCallFake(function() {
+        expect(mockJSONP.getRequestedUrl()).toBe(aerisUrl);
+      });
+
+      // mock aeris geocode
+      aerisService.geocode().done(callSpy);
+      mockJSONP.resolveWith(getSuccessResponse());
+
+      expect(callSpy).toHaveBeenCalled();
+      expect(callSpy).toHaveBeenCalledWith(getSuccessResponse());
+    });
+  });
 });
